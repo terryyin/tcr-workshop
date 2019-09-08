5466ac43c7fb8526a494dd7210d37831c48a585e
Stanly Lau <stanly@odd-e.com>
2019-09-08 15:15:50 +0800

    working

--- src/test/java/tcr/StringTemplateTest.java
+++ src/test/java/tcr/StringTemplateTest.java
@@ -1,4 +1,26 @@
 package tcr;
 
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class StringTemplateTest {
+    @Test
+    public void X() {
+        StringTemplate template = new StringTemplate("");
+        assertEquals("", template.replace(Collections.emptyList()));
+    }
+
+    private class StringTemplate {
+        public StringTemplate(String s) {
+
+        }
+
+        public String replace(List<Object> emptyList) {
+            return "";
+        }
+    }
 }
